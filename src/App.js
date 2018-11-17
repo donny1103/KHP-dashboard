d5761f52566573d96dda0a81c926fd6b65ac0f0e
donny1103 <donny1103@hotmail.com>
2018-11-17 12:06:33 -0800

    add features

--- src/App.js
+++ src/App.js
@@ -9,10 +9,18 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      queueData:''
+      queueData:'',
+      immediatePrioity:[],
+      highPrioity:[],
+      mediumPrioity:[],
+      lowPrioity:[],
+      toQueueData:''
     }
   }
 
+  handlePrioityClick = () => {
+    
+  }
   componentDidMount () {
     // setInterval(()=>{      
     //   axios.get('http://localhost:9000/pendinguser')
@@ -22,15 +30,32 @@ class App extends Component {
     // })}, 5000);
 
     axios.get('http://localhost:9000/pendinguser')
-    .then(data=>{
-    this.setState({queueData:data.data})})
+    .then(queue=>{
+      for(let key in queue.data){
+        if (queue.data[key].severity >= 1 && queue.data[key].severity < 25){
+          this.state.lowPrioity.push({...queue.data[key], id:key})
+        }
+        if (queue.data[key].severity >= 25 && queue.data[key].severity < 50){
+          this.state.mediumPrioity.push({...queue.data[key], id:key})
+        }
+        if (queue.data[key].severity >= 50 && queue.data[key].severity < 75){
+          this.state.highPrioity.push({...queue.data[key], id:key})
+        }
+        if (queue.data[key].severity >= 75 && queue.data[key].severity <= 100){
+          this.state.immediatePrioity.push({...queue.data[key], id:key})
+        }
+      }
+      this.setState({queueData:queue.data});
+      let toQueueData=this.state.immediatePrioity;
+      this.setState({toQueueData:toQueueData});
+    })
   }
 
   render() {
     return (
       <div className="App">
           <NavBar />
-          <Queue />
+          <Queue Data={this.state.toQueueData}/>
           <Chat />
       </div>
     );
--- src/components/Queue.jsx
+++ src/components/Queue.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/Card';
 
 const Queue = (props) => {
+  
   return (
     <div className="queue">
-      This is the queue
+      {props.Data? props.Data.map(item=>(
+        <Card key={item.id} >
+          <CardContent>
+            {item.severity}
+          </CardContent>
+        </Card>
+      )) : ''}
     </div>
   )
 };
