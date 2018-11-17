896a63a5432f7571927c58a31b3e3f7950c47e05
donny1103 <donny1103@hotmail.com>
2018-11-17 08:51:49 -0800

    update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -36,6 +36,7 @@ function NavBar(props) {
               <h3 class="priority-tab">
                   Very High Priority
               </h3>
+            </div>
           <div className="high-priority">
               <h3 class="priority-tab">
                   High Priority
@@ -59,40 +60,3 @@ function NavBar(props) {
 
 }
 export default NavBar;
-
-
-
-
-
-// import React, { Component } from 'react';
-
-// function Chatbar(props) {
-
-//     const currentUser = props.currentUser;
-
-//     // when the user presses enter after typing a message
-//     const onKeyPress = event => {
-//         if (event.key === "Enter") {
-//             props.addMessage(event.target.value);
-//             event.target.value = '';
-//         }
-//     }
-
-//     // when the users changes their name and presses enter
-//     const onUsernamePress = event => {
-//         if (event.key === "Enter") {
-//             props.updateUser(event.target.value);
-//         }
-//     }
-
-//     return (
-//         <div>
-//           <footer className="chatbar">
-//             <input defaultValue={currentUser.name} className="chatbar-username" placeholder="User name (optional)" onKeyPress={onUsernamePress} />
-//             <input className="chatbar-message" placeholder="Type a message and hit ENTER" onKeyPress={onKeyPress} />
-//           </footer>
-//         </div>
-//     );
-
-// }
-// export default Chatbar;
