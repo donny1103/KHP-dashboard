a316b447247112c000b975eb574bce7d1ce9a66e
donny1103 <donny1103@hotmail.com>
2018-11-17 13:47:21 -0800

    updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { Button } from 'antd';
+import { Button, Badge} from 'antd';
 import 'antd/dist/antd.css';
-import Badge from '@material-ui/core/Badge';
 
 class NavBar extends Component {
   constructor(props){
@@ -19,25 +18,29 @@ class NavBar extends Component {
         <div className="counsellor-profile">
             <img src="./img/profile_img.png" className="profile-img" />
         </div>
-
+        
         <Button className="nav-button immediate-priority" style={{marginBottom: "1.5rem", backgroundColor: "#D23827FF"}} value='1' onClick={this.onClick}>
+          <Badge count={this.props.Data.immediatePrioity ? this.props.Data.immediatePrioity.length : 0}>
             Immediate Priority
-            <Badge color="primary" badgeContent={4} className="queue-badge" />
+          </Badge>
         </Button>
           
         <Button className="nav-button high-priority" style={{marginBottom: "1.5rem", backgroundColor: "#D37B2CFF"}} value='2' onClick={this.onClick} >
-          High Priority
-          <Badge color="primary" badgeContent={4} className="queue-badge" />
+        <Badge count={this.props.Data.highPrioity ? this.props.Data.highPrioity.length : 0}>
+            High Priority
+          </Badge>
         </Button>
         
         <Button className="nav-button medium-priority" style={{marginBottom: "1.5rem", backgroundColor: "#CBA745FF"}} value='3' onClick={this.onClick}>
-          Medium Priority
-          <Badge color="primary" badgeContent={4} className="queue-badge" />
+        <Badge count={this.props.Data.mediumPrioity ? this.props.Data.mediumPrioity.length : 0}>
+            Medium Priority
+          </Badge>
         </Button>
 
         <Button className="nav-button low-priority" style={{backgroundColor: "#55AF39FF"}} value='4' onClick={this.onClick}>
-          Low Priority
-          <Badge color="primary" badgeContent={4} className="queue-badge" />
+        <Badge count={this.props.Data.lowPrioity ? this.props.Data.lowPrioity.length : 0}>
+            Low Priority
+          </Badge>
         </Button>
       </div>
 
