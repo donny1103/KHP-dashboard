8fc8950b8662d563526c79d098cab5a8c10a3d4b
Alfred Law <41144911+alfred529@users.noreply.github.com>
2018-11-18 10:38:29 -0500

    changing sad and scared rating

--- src/components/ChatBar.jsx
+++ src/components/ChatBar.jsx
@@ -5,7 +5,7 @@ const ChatBar = (props) => {
   return (
     <div className="chat-input">
       <Input style={{width: '35%', marginLeft: '5rem'}}/>
-      <Button type='primary'style={{width: '10%', marginLeft: '1rem'}} >Send</Button>
+      <Button type='primary'style={{width: '10%', marginLeft: '1rem', backgroundColor: '#0b95c8'}} >Send</Button>
     </div>
   )
 
--- src/components/Queue.jsx
+++ src/components/Queue.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Slider, Button, Collapse } from 'antd';
+import { Rate, Icon } from 'antd';
 
 const Panel = Collapse.Panel;
 
@@ -26,7 +27,7 @@ const Queue = (props) => {
         <Collapse bordered={false} defaultActiveKey={['1']} className="card">
           <Panel header={
             <div>
-              {item.gender ? <img src={`./img/${item.gender}.png`} className="card-icon" /> : 
+              {item.gender ? <img src={`./img/${item.gender}.png`} className="card-icon" /> :
                 <img src={`./img/question.png`} className="card-icon" />}
               <span>{item.name}, {item.age} yrs old</span><br />
               <span>Severity: {item.severity}</span>
@@ -34,8 +35,10 @@ const Queue = (props) => {
             </div>
           } key={item.id} style={customPanelStyle}>
             <p>favorite color: {item.favoriteColor}</p>
-            <p>Sad <Slider defaultValue={item.sadValue} min={0} max={7} marks={{0:'1', 7: '7'}}/> </p>
-            <p>Scared <Slider defaultValue={item.scaredValue} min={0} max={7} marks={{0:'1', 7: '7'}}/> </p>
+            <p>Sad <Slider defaultValue={item.sadValue} min={0} max={7} marks={{0:'1', 7: '7'}} disabled={true} className="slider"/> </p>
+            <i class="fas fa-ghost"></i>
+            <p>Scared <Slider defaultValue={item.scaredValue} min={0} max={7} marks={{0:'1', 7: '7'}} disabled={true} className="slider"/> </p>
+            <i class="fas fa-frown"></i>
             <p>Care About</p>
             <ul>
               {item.careAbout && item.careAbout.map(person=>(
@@ -43,8 +46,8 @@ const Queue = (props) => {
               ))}
             </ul>
 
-            <Button type='primary' >End Session</Button>
-            <Button type='primary' className="chat-button" value={item.name} onClick={clientName}>Chat ></Button>
+            <Button type='primary' style={{backgroundColor: '#0b95c8'}}>End Session</Button>
+            <Button type='primary' className="chat-button" value={item.name} onClick={clientName} style={{backgroundColor: '#0b95c8'}}>Chat ></Button>
           </Panel>
         </Collapse>
       ))}
