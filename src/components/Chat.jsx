bd3d3937f181ea3542db2fb701195a1b97aeb127
Alfred Law <41144911+alfred529@users.noreply.github.com>
2018-11-17 19:55:59 -0500

    changed card to collapse style

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -4,7 +4,7 @@ import Messages from './Messages.jsx';
 import './Chat.css'
 class Chat extends Component {
   state = {
-    messages:['Robbie: Hi, my name is Robbie','Dan: How are you Robbie?']
+    messages:['Hi, my name is AAA','How are you AAA?']
   }
 
   render () {
--- src/components/Queue.jsx
+++ src/components/Queue.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
-import { Card, Button} from 'antd';
+import { Card, Button, Collapse } from 'antd';
+
+const Panel = Collapse.Panel;
+
+const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`;
+
+const customPanelStyle = {
+  background: '#fff',
+  borderRadius: 4,
+  marginBottom: 24,
+  border: 0,
+  overflow: 'hidden',
+};
+
+
 
 const Queue = (props) => {
 
@@ -8,10 +26,25 @@ const Queue = (props) => {
   return (
     <div className="queue">
       {sortedData ? sortedData.map(item=>(
-        <Button key={item.id} className="card">
-            <img src="./img/boy.png" className="card-icon" /><span>{item.name}, {item.age} yrs old</span>
-            <p className="card-content">Severity: {item.severity}</p>
-        </Button>
+        // <Button key={item.id} className="card">
+        //     <img src="./img/boy.png" className="card-icon" /><span>{item.name}, {item.age} yrs old</span><br />
+        //     <span>Severity: {item.severity}</span>
+        //     <span className="wait-time">02:15</span>
+        // </Button>
+
+        <Collapse bordered={false} defaultActiveKey={['1']}>
+          <Panel header={
+            <div>
+              <img src="./img/boy.png" className="card-icon" /><span>{item.name}, {item.age} yrs old</span><br />
+              <span>Severity: {item.severity}</span>
+              <span className="wait-time">02:15</span>
+            </div>
+          } key={item.id} style={customPanelStyle}>
+            <p>Test here</p>
+          </Panel>
+
+        </Collapse>
+
       )) : ''}
     </div>
   )
