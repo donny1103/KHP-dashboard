30f522d34c6f448b12786e0aeb8e06c310244172
Alfred Law <41144911+alfred529@users.noreply.github.com>
2018-11-18 11:15:28 -0500

    queue card styling

--- src/components/Queue.jsx
+++ src/components/Queue.jsx
@@ -29,24 +29,24 @@ const Queue = (props) => {
             <div>
               {item.gender ? <img src={`./img/${item.gender}.png`} className="card-icon" /> :
                 <img src={`./img/question.png`} className="card-icon" />}
-              <span>{item.name}, {item.age} yrs old</span><br />
-              <span>Severity: {item.severity}</span>
-              <span className="wait-time">{Math.round((Date.parse(new Date()) - Date.parse(item.time))/60000)} mins ago</span>
+              <span className="text-font">{item.name}, {item.age} yrs old</span><br />
+              <span className="text-font">Severity: {item.severity}</span>
+              <span className="wait-time text-font">{Math.round((Date.parse(new Date()) - Date.parse(item.time))/60000)} mins ago</span>
             </div>
           } key={item.id} style={customPanelStyle}>
 
 
-            {item.favoriteColor && <p>favorite color: {item.favoriteColor}</p>}
-            {item.sadValue && <p>Sad <Slider defaultValue={item.sadValue} min={0} max={7} marks={{0:'1', 7: '7'}} disabled={false} className="slider"/> </p>}
-            {item.scaredValue && <p>Scared <Slider defaultValue={item.scaredValue} min={0} max={7} marks={{0:'1', 7: '7'}} disabled={true} className="slider"/> </p> }
-              {item.careAbout && <p>Care About</p>}
+            {item.favoriteColor && <p><b className="card-titles">Favorite color:</b> {item.favoriteColor}</p>}
+            {item.sadValue && <p><b className="card-titles">Sad</b> <Slider defaultValue={item.sadValue} min={0} max={7} marks={{0:'1', 7: '7'}} disabled={true} className="slider"/> </p>}
+            {item.scaredValue && <p><b className="card-titles">Scared</b> <Slider defaultValue={item.scaredValue} min={0} max={7} marks={{0:'1', 7: '7'}} disabled={true} className="slider"/> </p> }
+              {item.careAbout && <p><b className="card-titles">Care About</b></p>}
               <ul>
                 {item.careAbout && item.careAbout.map(person=>(
                   <li>{person}</li>
                 ))}
               </ul>
-            <Button type='primary' >End Session</Button>
-            <Button type='primary' className="chat-button" value={JSON.stringify({type: 'startChat',clientName: item.name, id: item.id, counsellorName: 'Dan Karres'})} onClick={clientName}>Chat ></Button>
+            <Button type='primary' style={{backgroundColor: '#0b95c8'}}>End Session</Button>
+            <Button type='primary' style={{backgroundColor: '#0b95c8'}} className="chat-button" value={JSON.stringify({type: 'startChat',clientName: item.name, id: item.id, counsellorName: 'Dan Karres'})} onClick={clientName}>Chat ></Button>
           </Panel>
         </Collapse>
       ))}
