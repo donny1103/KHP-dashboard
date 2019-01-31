74afa222752054df107e092cb013b310c08ccc6a
donny <donny1103@hotmail.com>
2019-01-31 09:41:06 -0500

    updated message scroll to buttom

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,25 +1,53 @@
-import React from 'react';
+import React, { Component } from "react";
 import uuidv4 from 'uuid/v4';
 
-const Messages = ({ clientName, messages }) => {
-  return ( 
-    clientName && messages ? 
-    (
+class Messages extends Component {
+
+  constructor(props){
+    super(props);
+  }
+
+  scrollToBottom = () => {
+    this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+  }
+
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+  
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+
+  render(){
+    let {clientName, messages} = this.props;
+    let displayName = clientName ? clientName : 'Anonymous';
+    return ( 
       <div className="messages">
-        <p>You are now chatting with <b>{clientName}</b></p><br />
-        {
-          messages.map(message=>(
-          <div key={uuidv4()}>
-            <div className={message.type==='toCounsellorMsg' ? 'message-time-left' : 'message-time-right'}> <span className='message-name'>{message.type==='toCounsellorMsg' ? clientName : 'Dan Karres'}</span> <span>{message.time}</span> </div> 
-            <div className={message.type==='toCounsellorMsg' ? 'message-bubbles-left' : "message-bubbles-right"}>
-              <span className={ message.type==='toCounsellorMsg' ? 'client-message' : "counsellor-message"}>{message.text}</span>
+        {messages ? 
+          (
+            <div>
+              <p>You are now chatting with <b>{displayName}</b></p><br />
+              {
+                messages.map(message=>(
+                <div key={uuidv4()}>
+                  <div className={message.type==='toCounsellorMsg' ? 'message-time-left' : 'message-time-right'}> <span className='message-name'>{message.type==='toCounsellorMsg' ? displayName : 'Dan Karres'}</span> <span>{message.time}</span> </div> 
+                  <div className={message.type==='toCounsellorMsg' ? 'message-bubbles-left' : "message-bubbles-right"}>
+                    <span className={ message.type==='toCounsellorMsg' ? 'client-message' : "counsellor-message"}>{message.text}</span>
+                  </div>
+                  </div>
+                ))
+              }
+              <div style={{ float:"left", clear: "both" }}
+                ref={(el) => { this.messagesEnd = el; }}>
+              </div>  
             </div>
-            </div>
-          ))
+          ) : null
         }
       </div>
-    ) : null
-  )
+    )
+  }
 }
 
 export default Messages;
+
